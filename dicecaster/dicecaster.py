f3d2cb9a1db472eaca1148f0f3f007193f649347
UpsilonG3 <thomas.k.rowland@proton.me>
2025-10-31 16:15:35 +1300

    Added DiceCaster class

--- dicecaster/dicecaster.py
+++ dicecaster/dicecaster.py
@@ -1,1 +1,12 @@
 import random
+from redbot.core import commands, Config
+
+
+class DiceCaster(commands.Cog):
+    def __init__(self, bot):
+        """Initialize the cog and register config defaults."""
+        self.bot = bot
+        # Use force_registration for safety and a unique identifier
+        self.config = Config.get_conf(
+            self, identifier=123456789, force_registration=True)
+        self.config.register_guild(active_game=None, guesses={})
