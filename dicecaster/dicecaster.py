c1ab97e6f99ccc5b2e744640e72de65c6d1531d7
UpsilonG3 <thomas.k.rowland@proton.me>
2025-11-02 18:51:35 +1300

    Setup for slash command and psuedo code for Cricket

--- dicecaster/dicecaster.py
+++ dicecaster/dicecaster.py
@@ -1,8 +1,34 @@
 import random
-from redbot.core import commands
+import discord
+from redbot.core import commands, app_commands
 
 
 class DiceCaster(commands.Cog):
     def __init__(self, bot):
         """Initialize the cog and register config defaults."""
         self.bot = bot
+
+    @app_commands.command()
+    @app_commands.guild_only()
+    async def roll(self, interaction: discord.Interaction, query: str = None):
+        """
+        Roll any combination of any number of dice, defaults to a single D20.
+        Users can type nothing, a single die, or many space-separated dice:
+          /roll
+          /roll 1D20
+          /roll 2D6 3D4
+        This cog is heavily inspired by DiceMaiden. The same logic can be found here: https://github.com/Humblemonk/DiceMaiden
+        """
+        if query == None:
+            # Roll a D20 and output the result.
+            return
+
+        # Seperate the input into sperate tokens, ie 1D20.
+        tokens = str.split(' ')  # Can be one single token.
+
+        # Iterate over the tokens.
+        # Validate the token.
+        # Roll for the token.
+        # Store the value outside the loop.
+
+        # Format and output the rolls.
